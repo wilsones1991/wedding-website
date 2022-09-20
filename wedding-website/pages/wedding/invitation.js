41bd8987abb6a392c3575d8876238c4f4043b27f
Eric Wilson <wilsones1991@gmail.com>
2022-09-20 15:28:56 -0700

    added an invitation page

---
+++ wedding-website/pages/wedding/invitation.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Invitation() {
+    return (
+        <>
+            <Head>
+                <title>Kylie and Eric's Wedding Invitation</title>
+            </Head>
+            <Invitation />
+        </>
+    )
+}
---
+++ wedding-website/src/Components/Invitation.js
@@ -0,0 +1,7 @@
+function Invitation() {
+    return (
+        <div className="invitation-container">
+            <img src="./wedding-invitation.png" alt="wedding invitation" />
+        </div>
+    )
+}
