953d3650711249646c2c764ecd06883901fdf445
Eric Wilson <wilsones1991@gmail.com>
2023-02-19 12:20:15 -0700

    Optimized image gallery

--- wedding-website/src/Components/GalleryComponent.js
+++ wedding-website/src/Components/GalleryComponent.js
@@ -3,18 +3,9 @@ import Image from 'next/image';
 
 export default function Gallery() {
 
-    const [images, setImages] = useState([]);
     const [imageIndex, setImageIndex] = useState(null);
 
     const closeupStyle = "position: fixed;top: 0;left: 0;right: 0;bottom: 0;background: rgba(15, 15, 15, .9);display: flex;flex-direction:column;justify-content: center;align-items: center;z-index: 1000000;overflowY: hidden;"
-
-    useEffect(() => {
-        fetch("https://wedding-website-server-360220.wl.r.appspot.com/api/gallery")
-            .then((response) => response.json())
-            .then((images) => {
-                setImages(images.images)
-            })
-    },[]);
 
     function handleCloseClick() {
         const closeup = document.querySelector(".closeupView");
@@ -24,17 +15,16 @@ export default function Gallery() {
     }
 
     function handleLeftClick() {
-        if (imageIndex === 0) {
-            setImageIndex(images.length - 1);
+        if (imageIndex === 1) {
+            setImageIndex(211);
         } else {
             setImageIndex(imageIndex - 1);
         }
     }
 
     function handleRightClick() {
-        if (imageIndex >= images.length - 1) {
-            console.log("yes");
-            setImageIndex(0);
+        if (imageIndex >= 211) {
+            setImageIndex(1);
         } else {
             setImageIndex(imageIndex + 1);
         }
@@ -48,57 +38,61 @@ export default function Gallery() {
         document.querySelector('body').style.overflow = 'hidden'
     }
 
-    function getImageUploader(index) {
-        return images[index]?.match(/^\S+\s(.+)\.\w+$/)[1] ?? "";
+    function setupIndexes() {
+        const indexes = [];
+        for (let index = 0; index < 211; index++) {
+            indexes.push(index + 1);
+        }
+        return indexes;
     }
-    // function handleMouseEnter(e) {
-    //     const caption = e.target.nextSibling
-    //     caption.setAttribute("style", "display:block;")
-    // }
 
-    // function handleMouseLeave(e) {
-    //     const caption = e.target.nextSibling
-    //     caption.setAttribute("style", "display:none;")
-    // }
+    function getPhotoCredit(index) {
+        if (index <= 211) {
+            return "Cat Fennell";
+        } else {
+            return "Eric Wilson";
+        }
 
-    const imageLoader = ({ src, quality }) => {
-        return `https://wedding-website-server-360220.wl.r.appspot.com/wedding-images/${src}?q=${quality || 75}`
-      }
+    }
 
     return (
         <>
             <div className="gallery">
-                {images.map((image, index) => {
-                    if (image != undefined) {
-                        console.log(image);
-                        return (
-                            <div className="galleryImage" key={index}>
-                                <Image className="galleryActualImg"
-                                layout="fill"
-                                src={imageLoader({src: image})}
-                                onClick={() => handleImgClick(index)} />
-                                <p className="caption">Photo Credit: {getImageUploader(index)}</p>
-                            </div>
-                        )
-                    }
-
+                {setupIndexes().map(index => {
+                    return (
+                    <div className="galleryImage" key={index}>
+                        <Image className="galleryActualImg"
+                        layout="fill"
+                        src={`/images/gallery-images/gallery-photo-${index}.jpeg`}
+                        placeholder="blur"
+                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOcMWOeDwAFugIc9oXdhAAAAABJRU5ErkJggg=="
+                        priority={index < 10 ? true : false}
+                        quality={50}
+                        onClick={() => handleImgClick(index)} />
+                    </div>
+                    )
                 })}
             </div>
             <div className="closeupView">
-                <div className="closeupCarousel">
+                <div className="closeupCloseButtonContainer">
                     <button className="galleryButton closeGallery" onClick={handleCloseClick}>X</button>
+                </div>
+                <div className="closeupCarousel">
                     <button className="galleryButton leftArrow" onClick={handleLeftClick}>&lt;</button>
                     <div className="galleryImgContainer">
-                        <img src={"https://wedding-website-server-360220.wl.r.appspot.com/wedding-images/" + images[imageIndex]} loading="lazy" />
+                        <Image
+                            layout="fill"
+                            src={`/images/gallery-images/gallery-photo-${imageIndex}.jpeg`}
+                            className="closeupImage" />
                     </div>
                     <button className="galleryButton rightArrow" onClick={handleRightClick}>&gt;</button>
                 </div>
                 <div className="imgMetadata">
                     <div>
-                        <p className="closeUpCaption">Photo Credit: {getImageUploader(imageIndex)}</p>
+                        <p className="closeUpCaption">Photo Credit: {getPhotoCredit(imageIndex)}</p>
                     </div>
                     <div>
-                        <p className="imgCount">Image {imageIndex + 1} of {images.length}</p>
+                        <p className="imgCount">Image {imageIndex + 1} of 211</p>
                     </div>
                 </div>
             </div>
